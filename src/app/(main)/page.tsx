893a5c56bfd59132e2ffc69aab3b6a6f6d66a82f
sunil8777 <suniljoshi8777@gmail.com>
2025-02-16 00:27:00 +0530

    models

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,28 @@
+import prisma from '@/lib/db'
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import { initialProfile } from "@/lib/initial-profile";
 import { UserButton } from "@clerk/nextjs";
+import { redirect } from 'next/navigation';
 
-export default function Home() {
+export default async function Home() {
+  
+  const profile = await initialProfile()
+  console.log(profile.id)
+
+  const server = await prisma.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id, 
+        }
+      }
+    }
+  });
+  
+  if(server){
+    return redirect(`/server/${server.id}`)
+  }
+  
   return (
     <div>
       <UserButton/>
---
+++ src/lib/initial-profile.ts
@@ -0,0 +1,33 @@
+import prisma from '@/lib/db'
+import { auth } from '@clerk/nextjs/server'
+import { currentUser } from '@clerk/nextjs/server'
+
+export async function initialProfile () {
+    const user = await currentUser()
+    const {  redirectToSignIn } = await auth()
+
+    if(!user){
+        return redirectToSignIn()
+    }
+
+    const profile = await prisma.profile.findUnique({
+        where:{
+            userId:user.id
+        }
+    })
+
+    if(profile){
+        return profile
+    }
+
+    const newProfile = await prisma.profile.create({
+        data:{
+            userId: user.id,
+            name: `${user.firstName} ${user.lastName}`,
+            email: user.emailAddresses[0].emailAddress,
+            profileImage: user.imageUrl
+        }
+    })
+
+    return newProfile
+}
